c9f0c35254322b9951f1388899bcd8b2798b133a
Ben Truyman <bentruyman@gmail.com>
2012-10-05 13:43:24 -0500

    Removed old compass compiler

--- app.js
+++ app.js
@@ -2,16 +2,13 @@
 // FIVEOCLOCKSONG
 ////////////////////////////////////////////////////////////////////////////////
 
-var CompassCompiler = require('./core/compass-compiler'),
-    LoadBalancer = require('./core/load-balancer'),
+var LoadBalancer = require('./core/load-balancer'),
     PollManager = require('./core/poll-manager'),
     repl = require('./core/repl');
 
-var compassCompiler = new CompassCompiler,
-    loadBalancer    = new LoadBalancer,
+var loadBalancer    = new LoadBalancer,
     pollManager     = new PollManager;
 
-// compassCompiler.init();
 pollManager.init();
 loadBalancer.start();
 repl.init();
