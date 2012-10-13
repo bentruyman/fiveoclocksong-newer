8247e04ede7514ccf26882fdc1c42ce538a8acd6
Ben Truyman <bentruyman@gmail.com>
2012-10-13 17:43:51 -0500

    No longer hard coding poll ID

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -12,7 +12,7 @@ define(
         
       },
       render: function () {
-        var poll = new Poll({ id: '20121010' }),
+        var poll = new Poll({ id: createDateString(new Date) }),
             pollView = new PollView({ model: poll });
         
         poll.fetch().then(function () {
@@ -22,7 +22,28 @@ define(
         return this;
       }
     });
-     
+    
+    function createDateString (date) {
+      date = new Date(date);
+      
+      var year  = date.getFullYear(),
+          month = date.getMonth() + 1,
+          day   = date.getDate();
+      
+      // stringify all the numbers
+      year =   year.toString();
+      month = month.toString();
+      day =     day.toString();
+      
+      // pad month to two digits
+      month = (month.length === 1) ? '0' + month : month;
+      
+      // pad day to two digits
+      day = (day.length === 1) ? '0' + day : day;
+      
+      return '' + year + month + day;
+    };
+    
     return AppView;
   }
 );
